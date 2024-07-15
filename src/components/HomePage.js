51be69c148435486a05e91327dcaadccde13914c
Gnanasampanthan Thivakar <g.thivakar123@gmail.com>
2024-07-15 19:06:20 +0530

    first commit

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,15 @@
-// src/components/HomePage.js
-
 import React from 'react';
 import { Link } from 'react-router-dom';
+import heroImage1 from '../images/back.jpg'; // Adjust the path as needed
 
 const HomePage = () => {
   return (
     <div>
       <h2>Welcome to PhoneBook App</h2>
+      <div
+        className="hero-image"
+        style={{ backgroundImage: `url(${heroImage1})`, height: '300px', backgroundSize: 'cover' }}
+      ></div>
       <p>This is the homepage content.</p>
       <Link to="/add-phone">
         <button>Add Phone Number</button>
--- src/components/PhoneList.js
+++ src/components/PhoneList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import backImage from '../images/back.jpg'; // Assuming back.jpg is in this path
 
 const PhoneList = () => {
   const [phoneNumbers, setPhoneNumbers] = useState([]);
@@ -19,7 +20,9 @@ const PhoneList = () => {
 
   return (
     <div>
+      <img src={backImage} alt="Background Image" style={{ width: '100%', marginBottom: '20px' }} />
       <h2>Phone Numbers</h2>
+     
       <ul>
         {phoneNumbers.map((phoneNumber) => (
           <li key={phoneNumber._id}>
